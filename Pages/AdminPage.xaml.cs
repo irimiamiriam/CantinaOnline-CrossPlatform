6b2684e4190994c3843c785c8dc9743e4bf3af4f
SfNicodim <snicodim65@gmail.com>
2025-03-10 08:38:31 +0200

    AdminPage

--- Pages/AdminPage.xaml.cs
+++ Pages/AdminPage.xaml.cs
@@ -5,5 +5,24 @@ public partial class AdminPage : ContentPage
 	public AdminPage()
 	{
 		InitializeComponent();
+		Title = "asasdasdasdasdasdd";
 	}
+    private void OnElevNouClicked(object sender, EventArgs e)
+    {
+        // Handle "Elev nou" button click
+        DisplayAlert("Elev nou", "Elev nou button clicked!", "OK");
+    }
+
+    private void OnPlataNouaClicked(object sender, EventArgs e)
+    {
+        // Handle "Plata noua" button click
+        DisplayAlert("Plata noua", "Plata noua button clicked!", "OK");
+    }
+
+    private void OnListaEleviClicked(object sender, EventArgs e)
+    {
+        // Handle "Lista elevi" button click
+        DisplayAlert("Lista elevi", "Lista elevi button clicked!", "OK");
+    }
+
 }
