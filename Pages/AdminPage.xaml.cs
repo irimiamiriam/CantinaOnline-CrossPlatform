fa9e0c4d50dc00d8b5f7d8e2dab2b01d1d2d4837
Miriam Irimia <irimiamiriam100@gmail.com>
2025-03-15 17:04:48 +0200

    nr de copii care mananca la cantina today

--- Pages/AdminPage.xaml.cs
+++ Pages/AdminPage.xaml.cs
@@ -1,3 +1,5 @@
+using CantinaOnline.ViewModel;
+
 namespace CantinaOnline.Pages;
 
 public partial class AdminPage : ContentPage
@@ -5,24 +7,9 @@ public partial class AdminPage : ContentPage
 	public AdminPage()
 	{
 		InitializeComponent();
-		Title = "asasdasdasdasdasdd";
-	}
-    private void OnElevNouClicked(object sender, EventArgs e)
-    {
-        // Handle "Elev nou" button click
-        DisplayAlert("Elev nou", "Elev nou button clicked!", "OK");
-    }
 
-    private void OnPlataNouaClicked(object sender, EventArgs e)
-    {
-        // Handle "Plata noua" button click
-        DisplayAlert("Plata noua", "Plata noua button clicked!", "OK");
+            BindingContext = new AdminPageViewModel();
     }
-
-    private void OnListaEleviClicked(object sender, EventArgs e)
-    {
-        // Handle "Lista elevi" button click
-        DisplayAlert("Lista elevi", "Lista elevi button clicked!", "OK");
-    }
+   
 
 }
---
+++ ViewModel/AdminPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CantinaOnline.ViewModel
+{
+    public class AdminPageViewModel : ObservableObject
+    {
+        private int _usersEatingToday;
+        public int UsersEatingToday
+        {
+            get => _usersEatingToday;
+            set => SetProperty(ref _usersEatingToday, value);
+        }
+
+        public AdminPageViewModel()
+        {
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            UsersEatingToday = await FirestoreService.GetUsersEatingToday();
+        }
+    }
+
+}
