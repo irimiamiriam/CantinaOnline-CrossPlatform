6d158de389e828c363b0812adae29a242591a61b
Miriam Irimia <irimiamiriam100@gmail.com>
2025-03-16 21:52:57 +0200

    added camera

--- Pages/AdminPage.xaml.cs
+++ Pages/AdminPage.xaml.cs
@@ -1,15 +1,29 @@
-using CantinaOnline.ViewModel;
+using Camera.MAUI.ZXingHelper;
+using Camera.MAUI;
+using CommunityToolkit.Mvvm.Input;
+
 
 namespace CantinaOnline.Pages;
 
 public partial class AdminPage : ContentPage
 {
-	public AdminPage()
-	{
-		InitializeComponent();
+    private AdminPageViewModel _viewModel;
 
-            BindingContext = new AdminPageViewModel();
+    public AdminPage()
+    {
+        InitializeComponent();
+        _viewModel = new AdminPageViewModel();
+        BindingContext = _viewModel;
+        _viewModel.SetCameraView(qrCamera);
     }
-   
 
+    private async void cameraView_CamerasLoaded(object sender, EventArgs e)
+    {
+        await ((AsyncRelayCommand)_viewModel.StartCameraCommand).ExecuteAsync(null);
+    }
+
+    private void cameraView_BarcodeDetected(object sender, BarcodeEventArgs args)
+    {
+        _viewModel.BarcodeDetectedCommand.Execute(args);
+    }
 }
--- ViewModel/AdminPageViewModel.cs
+++ ViewModel/AdminPageViewModel.cs
@@ -1,30 +1,94 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Camera.MAUI;
+using Camera.MAUI.ZXingHelper;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Dispatching;
+using Microsoft.Maui.Controls;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace CantinaOnline.ViewModel
+public partial class AdminPageViewModel : ObservableObject
 {
-    public class AdminPageViewModel : ObservableObject
+    private int _usersEatingToday;
+    public int UsersEatingToday
     {
-        private int _usersEatingToday;
-        public int UsersEatingToday
-        {
-            get => _usersEatingToday;
-            set => SetProperty(ref _usersEatingToday, value);
-        }
+        get => _usersEatingToday;
+        set => SetProperty(ref _usersEatingToday, value);
+    }
 
-        public AdminPageViewModel()
-        {
-            LoadData();
-        }
+    private CameraView _cameraView;
 
-        private async void LoadData()
+    [ObservableProperty]
+    private string barcodeResult;
+
+    public IRelayCommand StartCameraCommand { get; }
+    public IRelayCommand StopCameraCommand { get; }
+    public IRelayCommand<BarcodeEventArgs> BarcodeDetectedCommand { get; }
+
+
+    public AdminPageViewModel()
+    {
+        LoadData();
+        StartCameraCommand = new AsyncRelayCommand(StartCameraAsync);
+        StopCameraCommand = new AsyncRelayCommand(StopCameraAsync);
+        BarcodeDetectedCommand = new RelayCommand<BarcodeEventArgs>(OnBarcodeDetected);
+    }
+
+    private async void LoadData()
+    {
+        UsersEatingToday = await FirestoreService.GetUsersEatingToday();
+    }
+    public void SetCameraView(CameraView cameraView)
+    {
+        _cameraView = cameraView;
+    }
+
+    private async Task StartCameraAsync()
+    {
+        if (_cameraView == null || _cameraView.Cameras.Count == 0)
+            return;
+
+        _cameraView.Camera = _cameraView.Cameras.First();
+        await _cameraView.StopCameraAsync();
+        await _cameraView.StartCameraAsync();
+    }
+
+    private async Task StopCameraAsync()
+    {
+        if (_cameraView != null)
         {
-            UsersEatingToday = await FirestoreService.GetUsersEatingToday();
+            await _cameraView.StopCameraAsync();
         }
+    }
+
+    private void OnBarcodeDetected(BarcodeEventArgs args)
+    {
+        if (args.Result?.Any() == true)
+        {
+            BarcodeResult = $"{args.Result[0].BarcodeFormat}: {args.Result[0].Text}";
+
+            // Process scanned QR Code
+            CheckUserPayment(args.Result[0].Text);
+        }
+    }
+
+    private async void CheckUserPayment(string userId)
+    {
+        bool isPaid = await CheckIfUserPaid(userId);
+        string message = isPaid ? "Utilizatorul a plătit" : "Zi neplătită";
+        await Application.Current.MainPage.DisplayAlert("Rezultat Scanare", message, "OK");
+    }
+
+    private async Task<bool> CheckIfUserPaid(string userId)
+    {
+        var user = await FirestoreService.GetUserById(userId);
+        if (user != null)
+        {
+            DateTime today = DateTime.Now.Date;
+            return user.ZilePlatite.ContainsKey(today) && user.ZilePlatite[today] == 0;
+        }
+        return false;
     }
 
 }
